3f3c8ddf3acc116062c17e65170cc1ae7fd76685
augustofadel <augustofadel@gmail.com>
2020-07-27 16:18:16 -0300

    bacen: primeira versão funcional

--- ui/app_test.R
+++ ui/app_test.R
@@ -3,12 +3,20 @@
 # https://mastering-shiny.org/
 
 
+# config ------------------------------------------------------------------
+
 library(shiny)
 library(shinydashboard)
 library(shinyFiles)
+# library(shinyalert)
 library(lubridate)
+library(purrr)
+library(stringr)
 
-dir_gcad <- '//WARQPRD14V/cempre/GCAD/REGISTRO_ADMINISTRATIVO'
+source('../scripts/bacen.R')
+
+gcad_dir <- '//WARQPRD14V/cempre/GCAD/REGISTRO_ADMINISTRATIVO'
+
 
 ui <- dashboardPage(
   
@@ -25,149 +33,130 @@ ui <- dashboardPage(
     )
   ),
   
-  # dashboardBody(
-  #   # Boxes need to be put in a row (or column)
-  #   fluidRow(
-  #     box(plotOutput('plot1', height = 250)),
-  #     
-  #     box(
-  #       title = 'Controls',
-  #       sliderInput('slider', 'Number of observations:', 1, 100, 50)
-  #     )
-  #   )
-  # )
   dashboardBody(
     tabItems(
-      
+# ui :: bacen -------------------------------------------------------------
       tabItem(
         tabName = 'bacen',
-        h2('Relação de instituições em funcionamento no país'),
-        h5(
-          'Fonte: Banco Central do Brasil', 
-          tags$a(
-            href = 'https://www.bcb.gov.br/estabilidadefinanceira/relacsaveao_instituicoes_funcionamento', 
-            icon('link')
-          )
-        ),
-        fluidRow(
-          box(
-            checkboxGroupInput(
-              inputId = 'tipo_bacen1', 
-              label = 'Tipos de instituições', 
-              choices = 
-                list(
-                  'conglomerados', 
-                  'bancos', 
-                  'cooperativas de crédito' = 'cooperativas', 
-                  'sociedades', 
-                  'administradoras de consórcios' = 'consorcios'
-                ), 
-              selected = 
-                c(
-                  'conglomerados', 
-                  'bancos', 
-                  'cooperativas', 
-                  'sociedades', 
-                  'consorcios'
-                ),
-              width = NULL, 
-              choiceNames = NULL,
-              choiceValues = NULL
+        h1('BACEN'),
+        box(
+          h3('Relação de instituições em funcionamento no país'),
+          h5(
+            'Fonte: Banco Central do Brasil', 
+            tags$a(
+              href = 'https://www.bcb.gov.br/estabilidadefinanceira/relacsaveao_instituicoes_funcionamento', 
+              icon('link')
             )
           ),
-          box(
-            dateRangeInput(
-              inputId = 'periodo', 
-              label = 'Período',
-              start = lubridate::today() - months(1), 
-              end = lubridate::today() - months(1), 
-              min = '2007-01-01',
-              max = lubridate::today(), 
-              format = 'mm-yyyy', 
-              startview = 'year',
-              weekstart = 0, 
-              language = 'pt-BR', 
-              separator = ' a '
+          tags$br(),
+          checkboxGroupInput(
+            inputId = 'bacen_tipo1', 
+            label = 'Tipos de instituições', 
+            choices = 
+              list(
+                'conglomerados', 
+                'bancos', 
+                'cooperativas de crédito' = 'cooperativas', 
+                'sociedades', 
+                'administradoras de consórcios' = 'consorcios'
+              ), 
+            selected = 
+              c(
+                'conglomerados', 
+                'bancos', 
+                'cooperativas', 
+                'sociedades', 
+                'consorcios'
+              ),
+            width = NULL, 
+            choiceNames = NULL,
+            choiceValues = NULL
+          ),
+          
+          tags$hr(),
+          
+          h3('Relação de agências, postos e filiais de administradoras de consórcio'),
+          h5(
+            'Fonte: Banco Central do Brasil', 
+            tags$a(
+              href = 'https://www.bcb.gov.br/estabilidadefinanceira/agenciasconsorcio', 
+              icon('link')
             )
+          ),
+          tags$br(),
+          checkboxGroupInput(
+            inputId = 'bacen_tipo2', 
+            label = 'Tipos de instituições', 
+            choices = 
+              list(
+                'agências' = 'agencias', 
+                'postos de atendimento' = 'postos', 
+                'postos de atendimento eletrônico' = 'pae', 
+                'filiais de administradoras de consórcio' = 'filiais_adm_consorcios'
+              ), 
+            selected = 
+              c(
+                'agencias', 
+                'postos', 
+                'pae', 
+                'filiais_adm_consorcios'
+              ),
+            width = NULL, 
+            choiceNames = NULL,
+            choiceValues = NULL
           )
         ),
-        tags$hr(),
         
-        h2('Relação de agências, postos e filiais de administradoras de consórcio '),
-        h5(
-          'Fonte: Banco Central do Brasil', 
-          tags$a(
-            href = 'https://www.bcb.gov.br/estabilidadefinanceira/agenciasconsorcio', 
-            icon('link')
-          )
-        ),
-        fluidRow(
-          box(
-            checkboxGroupInput(
-              inputId = 'tipo_bacen1', 
-              label = 'Tipos de instituições', 
-              choices = 
-                list(
-                  'agências' = 'agencias', 
-                  'postos de atendimento' = 'postos', 
-                  'postos de atendimento eletrônico' = 'pae', 
-                  'filiais de administradoras de consórcio' = 'filiais_adm_consorcios'
-                ), 
-              selected = 
-                c(
-                  'agencias', 
-                  'postos', 
-                  'pae', 
-                  'filiais_adm_consorcios'
-                ),
-              width = NULL, 
-              choiceNames = NULL,
-              choiceValues = NULL
-            )
+        box(
+          dateRangeInput(
+            inputId = 'bacen_periodo', 
+            label = 'Período',
+            start = lubridate::today() - months(1), 
+            end = lubridate::today() - months(1), 
+            min = '2007-01-01',
+            max = lubridate::today(), 
+            format = 'mm-yyyy', 
+            startview = 'year',
+            weekstart = 0, 
+            language = 'pt-BR', 
+            separator = ' a '
           ),
-          box(
-            dateRangeInput(
-              inputId = 'periodo', 
-              label = 'Período',
-              start = lubridate::today() - months(1), 
-              end = lubridate::today() - months(1), 
-              min = '2007-01-01',
-              max = lubridate::today(), 
-              format = 'mm-yyyy', 
-              startview = 'year',
-              weekstart = 0, 
-              language = 'pt-BR', 
-              separator = ' a '
-            )
-          )
-        ),
-        tags$hr(),
-        
-        fluidRow(
-          column(
-            6,
-            shinyDirButton(
-              id = 'dir_bacen', 
-              label = 'Diretório*', 
-              title = 'Salvar em...',
-              icon = icon('folder-open')
-            ),
-            verbatimTextOutput('dir_bacen', placeholder = TRUE),
-            helpText('*cada tipo de instituição é armazenado em um subdiretório')
+          checkboxGroupInput(
+            inputId = 'bacen_ext', 
+            label = 'Arquivos de saída', 
+            choices = list('csv', 'txt' = 'tsv', 'xlsx'), 
+            selected = 'tsv',
+            inline = TRUE
           ),
-          column(
-            2,
-            actionButton(
-              inputId = 'exec_bacen',
-              label = 'Executar',
-              icon = icon('download')
-            ),
-            # submitButton('Submit'))
-            offset = 2
+          # TODO: substituir textInput por shinyDirButton para selecionar diretório
+          # shinyDirButton(
+          #   id = 'bacen_dir_sel', 
+          #   label = 'Salvar em', 
+          #   title = 'Salvar em...',
+          #   FALSE,
+          #   icon = icon('folder-open')
+          # ),
+          textInput(
+            inputId = 'bacen_dir_sel',
+            label = 'Diretório',
+            value = file.path(gcad_dir, 'BACEN/ORIGINAL'),
+            placeholder = TRUE
+          ),
+          verbatimTextOutput('bacen_dir', placeholder = TRUE),
+          helpText('*cada tipo de instituição é armazenado em um subdiretório'),
+          actionButton(
+            inputId = 'bacen_exec',
+            label = 'Executar',
+            icon = icon('download')
           )
-        )
+          # submitButton('Submit'))
+        ),
+        # TODO: apagar output texto teste
+        verbatimTextOutput('test_text', placeholder = TRUE),
+        # TODO: melhorar visualização tabela final para incluir url (linhas separada?)
+        box(dataTableOutput('bacen_tab'))
       ),
-      
+# ui :: cadastur ----------------------------------------------------------
       tabItem(
         tabName = 'cadastur',
         h2('Cadastro dos prestadores de serviços turísticos'),
@@ -209,24 +198,112 @@ server <- function(input, output) {
     hist(data)
   })
   
+  dirs <- reactiveValues()
   
-  shinyDirChoose(
-    input = input,
-    id = 'dir_bacen',
-    roots = c(home = file.path(dir_gcad, 'bacen'))
-  )
-  global <- reactiveValues(datapath = dir_gcad)
-  dir_bacen <- reactive(input$dir_bacen)
-  output$dir_bacen <- renderText({global$datapath})
-  observeEvent(
-    ignoreNULL = TRUE,
-    eventExpr = {input$dir_bacen},
-    handlerExpr = {
-      if (!"path" %in% names(dir_bacen())) 
-        return()
-      global$datapath <- file.path(dir_gcad, dir_bacen()$path[-1])
-    }
-  )
+
+
+
+# bacen -------------------------------------------------------------------
+
+  bacen_periodo <-
+    reactive({
+      seq(input$bacen_periodo[1], input$bacen_periodo[2], by = 'months') %>%
+        str_sub(1, 7)
+    })
+  bacen_tipo <- reactive(c(input$bacen_tipo1, input$bacen_tipo2))
+  # bacen_ext <- reactive(input$bacen_ext)
+  # input$bacen_dir_sel
+  # input$bacen_dir
+  # input$bacen_exec
+  
+  # output$test_text <-
+  #   renderText({bacen_ext()})
+    # renderPrint({bacen_ext()})
+  
+  
+  output$bacen_dir <- renderText({input$bacen_dir_sel})
+  
+  bacen_result <- eventReactive(input$bacen_exec, {
+    expand.grid(
+      tipo = bacen_tipo(),
+      periodo = bacen_periodo(),
+      stringsAsFactors = F
+    ) %>%
+      tibble::as_tibble() %>%
+      purrr::pmap_dfr(
+        function(tipo, periodo) {
+          bacen(periodo, tipo, input$bacen_dir_sel, input$bacen_ext)
+        }
+      )
+  })
+  
+  output$bacen_tab <- 
+    renderDataTable(
+      # TODO: incluir url consultada  na tabela final
+      dplyr::select(bacen_result(), -url), 
+      options = 
+        list(
+          lengthMenu = list(c(5, 10), c('5', '10')),
+          pageLength = 5,
+          searching = FALSE
+        )
+    )
+
+
+  
+  
+  
+  # shinyDirChoose(
+  #   input = input,
+  #   id = 'bacen_dir_sel',
+  #   roots = c(home = file.path(gcad_dir, 'bacen'))
+  # )
+  # output$bacen_dir <- renderText({gcad_dir})
+  
+  # shinyDirChoose(
+  #   input = input,
+  #   id = 'bacen_dir_sel',
+  #   roots = c(home = file.path(gcad_dir, 'bacen'))
+  # )
+  # dirs$bacen <- reactive(input$bacen_dir)
+  # # bacen_dir <- reactive(input$bacen_dir)
+  # output$bacen_dir <- renderText({dirs$bacen})
+  # # observeEvent(
+  # #   ignoreNULL = TRUE,
+  # #   eventExpr = {input$bacen_dir},
+  # #   handlerExpr = {
+  # #     if (!'path' %in% names(bacen_dir())) 
+  # #       return()
+  # #     global$datapath <- file.path(gcad_dir, bacen_dir()$path[-1])
+  # #   }
+  # # )
+  
+  
+  
+  # input$bacen_tipo1
+  # input$bacen_tipo2
+  # input$bacen_periodo
+  # input$bacen_dir_sel
+  # input$bacen_dir
+  # input$bacen_ext
+  # input$bacen_exec
+  
+  # ano <- 2007:2020
+  # purrr::walk2(
+  #   rep(str_pad(1:12, 2, 'left', '0'), length(ano)), 
+  #   rep(ano, each = 12),
+  #   function(mes, ano) {
+  #     if (as_date(paste(ano, mes, 1, sep = '-')) < (today() - months(1))) {
+  #       bacen(
+  #         ano = ano, 
+  #         mes = mes, 
+  #         # tipo = c('postos', 'pae'), 
+  #         saida = 'tsv'
+  #       )
+  #       Sys.sleep(runif(1, 5, 10))
+  #     }
+  #   }
+  # )
   
   
   
