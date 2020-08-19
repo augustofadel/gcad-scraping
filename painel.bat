96994381801fed09a8099413f55f654d00081ae1
augustofadel <augustofadel@gmail.com>
2020-08-19 14:20:31 -0300

    alteração verificação pacotes necessários instalados, possibilidade de instalar múltiplos pacotes

--- painel.bat
+++ painel.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\R\R-4.0.0\bin\R.exe" -e "if(all(installed.packages()[,'Package'] != 'shiny')) install.packages('shiny', repos = 'http://cran.rstudio.com');shiny::runApp('//WARQPRD14V/cempre/GCAD/REGISTRO_ADMINISTRATIVO/gcad-scraping/ui/app.R', launch.browser = TRUE)"
+"C:\Program Files (x86)\R\R-4.0.0\bin\R.exe" -e "pckg <- c('shiny');for (p in pckg) {if(all(p != installed.packages()[,'Package'])) install.packages(p, repos = 'http://cran.rstudio.com')};shiny::runApp('//WARQPRD14V/cempre/GCAD/REGISTRO_ADMINISTRATIVO/gcad-scraping/ui/app.R', launch.browser = TRUE)"
